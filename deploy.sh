9c4e39e8f1644c54330a9ce204d47d9c1ca1bccf
saifudheen <saifudheen@innovatise.com>
2022-03-09 14:56:27 +0530

    modified

--- deploy.sh
+++ deploy.sh
@@ -5,17 +5,18 @@ SWREGION="$2"
 TableName="$3"
 StageName="$4"
 Email="$5"
+BucketName="$6"
 # yaml2json swagger.yaml | jq . > output/swagger_json.json
 # sed -ie 's/SWREGION/'${SWREGION}'/g' output/swagger_json.json
 
 aws cloudformation package          \
-    --s3-bucket mycloud-saifu1             \
+    --s3-bucket $BucketName             \
     --template-file template.yaml    \
     --region $SWREGION               \
     --output-template-file output/output.yaml 
 
 aws cloudformation deploy \
-  --s3-bucket mycloud-saifu1       \
+  --s3-bucket $BucketName       \
   --template-file output/output.yaml \
   --stack-name WorkshopStack \
   --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
